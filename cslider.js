59f3e4dbaad0290099a4aeb34e5a2f222c3e19b6
ChinhPhan <chinhphan099@gmail.com>
2016-11-17 17:15:27 +0700

    17

--- cslider.js
+++ cslider.js
@@ -220,31 +220,17 @@
       dots: true,
       infinite: true,
       speed: 600,
-      slidesToShow: 4,
+      slidesToShow: 3,
       slidesToScroll: 1,
       // autoplay: true,
       // autoplaySpeed: 3000,
       rtl: $('html').attr('dir') === 'rtl' ? true : false,
       responsive: [
         {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3
-          }
-        },
-        {
           breakpoint: 544,
           settings: {
             slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToScroll: 1
           }
         },
         {
